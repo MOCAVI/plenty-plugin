e464e3b1a1a7a1a529150100c0c56e5d583f87ca
MOCAVI <32903381+MOCAVI@users.noreply.github.com>
2017-10-19 08:29:07 +0200

    HelloWorld v0.1

---
+++ src/Providers/HelloWorldServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+public function map(Router $router)
+  {
+      $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+  }
+
+
+namespace HelloWorld\Providers;
+
+
+use Plenty\Plugin\ServiceProvider;
+
+class HelloWorldServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     */
+
+    public function register()
+    {
