8b613deb9ff4aaa2be33e5f73cb939397562eeb8
MOCAVI <32903381+MOCAVI@users.noreply.github.com>
2017-10-19 09:06:17 +0200

    Hello World v0.2

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -1,20 +1,14 @@
 <?php
-
 public function register()
        {
            $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
        }
-
 namespace HelloWorld\Providers;
-
-
 use Plenty\Plugin\RouteServiceProvider;
 use Plenty\Plugin\Routing\Router;
-
 class HelloWorldRouteServiceProvider extends RouteServiceProvider
 {
     public function map(Router $router)
     {
-
     }
 }
